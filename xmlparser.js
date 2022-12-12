71b2bc26a3d049e4e5fcf6daca58e0af1ca6c277
HuuTrucNguyen <trnguyen997@gmail.com>
2022-12-12 16:34:20 +0700

    XMLFile

--- xmlparser.js
+++ xmlparser.js
@@ -8,29 +8,29 @@ const app = express();
 
 const parser = new xml2js.Parser();
 
-const ReadXMLFile = (req,res)=>{
+const getXMLFile = (req,res)=>{
     try {
         fs.readFile('Sensor.xml', (err, data) => {
             parser.parseString(data, (err, result) => {
-                console.log(util.inspect(result, false, null, true));
-                // let {houseIds} = req.query;
-                // every some map filter find
-                const houseIds = '1,2'
+                console.log(util.inspect(result, false, null, true))
+                // console.log(JSON.parse(data)['SensorDHT'])
+                let {houseIds} = req.query;
+                // const houseIds = '1,2'
                 let chosenSensors= [];
                 let chosenHouseIds = houseIds.split(',');
                 for(let i =0; i< result.length ; i++){
                     if(result[i].houseId.every(i => chosenHouseIds.includes(i))){
                         chosenSensors.push(sensors[i])
-
                     }
                 } 
+            res.status(200).json({sensors});    
             });
         });
-        console.log('connect successfully')
-        // next()
     } catch (error) {
         console.log(error)
+        res.status(500).json({msg:error.message});
     }
  }
 
- ReadXMLFile()
+//  getXMLFile()
+module.exports = getXMLFile;
